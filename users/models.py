edb1e754d7d98e5f03b7c8f08121213926f846e9
danil <shuvalov58@icloud.com>
2024-04-06 18:27:30 +0300

    добавлена модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,6 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
 
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    phone = models.CharField(max_length=100, verbose_name="Телефон", **NULLABLE)
+    is_active = models.BooleanField(default=True)
+    telegram_id = models.CharField(max_length=100, verbose_name="Телеграмм",**NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
