ecd15516c4539b0e1470b59888dc6f3c8b906f3e
Danila Shuvalov <shuvalov58@icloud.com>
2024-04-23 06:08:00 +0300

    исправление установки связи между бд и переменными

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM python:3
+FROM python:3.10-slim
 
 WORKDIR /code
 
+RUN pip install --upgrade pip
+
 COPY ./requirements.txt /code/
 
-RUN pip install -r requirements.txt
+RUN pip install -r /code/requirements.txt
 
 COPY . .
--- config/settings.py
+++ config/settings.py
@@ -85,16 +85,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST')
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
